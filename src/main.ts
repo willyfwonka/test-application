9dc9b9f9619161e1db28f7d5913ca8d2f3d4a83a
Oguz Turkay <oguz@arthylabs.com>
2023-02-17 18:48:43 +0300

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log(process.env);
+
   await app.listen(process.env.SERVER_PORT || 4000);
 }
 bootstrap();
